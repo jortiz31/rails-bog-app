8f29edff18ef840c968cb3be27cb4f7c450eef41
Jonathan <jortiz@tunein.com>
2016-04-29 17:22:15 -0700

    create working

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   get "/creatures", to: "creatures#index", as: "creatures"
   get "/creatures/new", to: "creatures#new"
+  post "/creatures", to: "creatures#create"
+  
 
 end
