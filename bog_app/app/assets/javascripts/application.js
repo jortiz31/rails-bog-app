3902df2e04740a1fbc7ea9c85df9e5fd0d48e322
Jonathan <jortiz@tunein.com>
2016-05-01 11:07:35 -0700

    working on jquery add

--- bog_app/app/assets/javascripts/application.js
+++ bog_app/app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
+// app/assets/javascripts/application.js
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -10,6 +12,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
