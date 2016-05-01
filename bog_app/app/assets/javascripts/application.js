3117849aa2d4fe60774ed159c8425f6268bb17f6
Jonathan <jortiz@tunein.com>
2016-05-01 11:54:43 -0700

    give up on jquery

--- bog_app/app/assets/javascripts/application.js
+++ bog_app/app/assets/javascripts/application.js
@@ -1,5 +1,3 @@
-// app/assets/javascripts/application.js
-
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -12,8 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-
-//= require jquery
-//= require jquery_ujs
+//= require bootstrap.min
 //= require turbolinks
 //= require_tree .
