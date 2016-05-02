78a6e905bbc82a32a6ed66bdff46c9f740daff31
Jonathan <jortiz@tunein.com>
2016-05-01 21:49:24 -0700

    create working

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -7,6 +7,17 @@ class CreaturesController < ApplicationController
   end
 
   def new
+    @creature = Creature.new
     render :new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature = Creature.new(creature_params)
+
+    if creature.save
+      redirect_to creatures_path
+    end
   end
 end
